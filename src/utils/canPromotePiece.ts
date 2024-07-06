8b39195539e6e0d82a55fbdbb896f4a6000c0eae
Josh <taylorjoshua099@gmail.com>
2024-07-06 20:18:54 +1000

    Fix pawn promotion functionality

--- src/utils/canPromotePiece.ts
+++ src/utils/canPromotePiece.ts
@@ -1,4 +1,3 @@
-import { INITIAL_GAME_STATE } from "../constants/InitialGameState";
 import { Piece } from "../interfaces/Piece";
 import { Position } from "../interfaces/Position";
 
@@ -6,12 +5,10 @@ export function canPromotePiece(piece: Piece, targetPosition: Position): boolean
   if (piece.type !== 'pawn')
     return false;
 
-  const { y: initialY } = INITIAL_GAME_STATE.pieces[piece.id];
-
-  if (initialY === 2 && targetPosition.y === 8)
+  if (piece.colour === 'white' && targetPosition.y === 8)
     return true;
 
-  if (initialY === 7 && targetPosition.y === 1)
+  if (piece.colour === 'black' && targetPosition.y === 1)
     return true;
 
   return false;
