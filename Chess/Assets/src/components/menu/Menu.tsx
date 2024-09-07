dda2e0c55a7595e17b6622bad1ee684b6cbb4cdd
Josh <taylorjoshua099@gmail.com>
2024-09-07 20:15:54 +1000

    Get move numbers in the side panel showing accurately

--- Chess/Assets/src/components/menu/Menu.tsx
+++ Chess/Assets/src/components/menu/Menu.tsx
@@ -9,22 +9,41 @@ function getColourString(colour: PieceColour): string {
   return 'Light';
 }
 
+type Moves = Array<[ number, string, string ]>;
+
 interface MenuProps {
   state: GameState;
   piecePositionMap: PiecePositionMap;
 }
 
 export function Menu(props: MenuProps): JSX.Element {
+  const moves: Moves = [];
+
+  for (let i = 0; i < props.state.moves.length; i++) {
+    if (i % 2 === 0) {
+      moves.push([ (i + 2) / 2, props.state.moves[i], '' ]);
+      continue;
+    }
+
+    moves[moves.length - 1][2] = props.state.moves[i];
+  }
+
   return (
     <div className='w-60 max-h-[768px] min-h-[768px] rounded-lg border border-chess-dark text-chess-dark bg-chess-light ms-4 font-semibold overflow-y-scroll'>
       <div className='p-2'>
         <h2>Moves</h2>
 
         <ul>
-          {props.state.moves.map((move, index) => {
+          {moves.map(([ moveNumber, lightMove, darkMove ]) => {
             return (
-              <li key={index}>
-                {index + 1}. {move}
+              <li key={moveNumber} className='columns-7'>
+                <div>{moveNumber}.</div>
+                <div>{lightMove}</div>
+                <div>&nbsp;</div>
+                <div>&nbsp;</div>
+                <div>{darkMove}</div>
+                <div>&nbsp;</div>
+                <div>&nbsp;</div>
               </li>
             );
           })}
