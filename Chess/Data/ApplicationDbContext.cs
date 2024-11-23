965d93be6358e10eb6829213000d0600f38c328d
Josh <taylorjoshua099@gmail.com>
2024-11-23 15:18:37 +1100

    Seed time controls

--- Chess/Data/ApplicationDbContext.cs
+++ Chess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Chess.Models.Entities;
+﻿using Chess.Enums;
+using Chess.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Chess.Data;
@@ -17,5 +18,28 @@ public class ApplicationDbContext : DbContext
         optionsBuilder
             .UseSqlite($"Data Source={dbPath}")
             .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var timeControls = new TimeControl[]
+        {
+            // Bullet time controls
+            new() { Type = TimeControlType.Bullet, Time = 100000, Increment = 0 },
+            new() { Type = TimeControlType.Bullet, Time = 100000, Increment = 1000 },
+            new() { Type = TimeControlType.Bullet, Time = 200000, Increment = 1000 },
+
+            // Blitz time controls
+            new() { Type = TimeControlType.Blitz, Time = 180000, Increment = 0 },
+            new() { Type = TimeControlType.Blitz, Time = 180000, Increment = 2000 },
+            new() { Type = TimeControlType.Blitz, Time = 300000, Increment = 0 },
+            
+            // Rapid time controls
+            new() { Type = TimeControlType.Rapid, Time = 600000, Increment = 0 },
+            new() { Type = TimeControlType.Rapid, Time = 900000, Increment = 10000 },
+            new() { Type = TimeControlType.Rapid, Time = 1800000, Increment = 0 }
+        };
+
+        modelBuilder.Entity<TimeControl>().HasData(timeControls);
     }
 }
---
+++ Chess/Migrations/20241123041703_SeedTimeControls.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Chess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTimeControls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TimeControls",
+                columns: new[] { "Increment", "Time", "Type" },
+                values: new object[,]
+                {
+                    { 0ul, 100000ul, 0 },
+                    { 1000ul, 100000ul, 0 },
+                    { 0ul, 180000ul, 1 },
+                    { 2000ul, 180000ul, 1 },
+                    { 1000ul, 200000ul, 0 },
+                    { 0ul, 300000ul, 1 },
+                    { 0ul, 600000ul, 2 },
+                    { 10000ul, 900000ul, 2 },
+                    { 0ul, 1800000ul, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 0ul, 100000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 1000ul, 100000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 0ul, 180000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 2000ul, 180000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 1000ul, 200000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 0ul, 300000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 0ul, 600000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 10000ul, 900000ul });
+
+            migrationBuilder.DeleteData(
+                table: "TimeControls",
+                keyColumns: new[] { "Increment", "Time" },
+                keyValues: new object[] { 0ul, 1800000ul });
+        }
+    }
+}
